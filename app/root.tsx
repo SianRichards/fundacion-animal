6c948b6eefa68d52222c58f9478615b46e9ed485
Sian Richards <sian_richards@hotmail.co.uk>
2023-10-16 13:39:30 +0100

    initial commit

---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import styles from "./styles/tailwind.css";
+
+// links should be exported here if you want to add content to <Links /> under <head>
+// e.g. traditional HTML: <link rel="stylesheet" href="stylesheet_pathway">
+export const links: LinksFunction = () => [
+  ...[{ rel: "stylesheet", href: styles }],
+];
+
+// routes live inside of <Outlet />
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
