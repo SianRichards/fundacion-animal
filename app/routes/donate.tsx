a94518b35b172d2fbc0cc6448cb592a92ae390e6
Sian Richards <sian_richards@hotmail.co.uk>
2023-10-17 15:03:20 +0100

    donate page

---
+++ app/routes/donate.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "~/components/nav-bar";
+
+const Donate = () => {
+  return (
+    <div>
+      <NavBar />
+      Donate
+    </div>
+  );
+};
+
+export default Donate;
