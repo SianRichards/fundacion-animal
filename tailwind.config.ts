16a3934b78a6c91605c57ee6258f6db87391aa8b
Sian Richards <sian_richards@hotmail.co.uk>
2023-10-17 15:02:35 +0100

    add responsiveness classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,11 @@
 export default {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
+    screens: {
+      "mobile": "350px",
+      "tablet": "700px",
+      "desktop": "1024px",
+    },
     extend: {},
   },
   plugins: [],
