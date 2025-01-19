b2765a869ca8482abc9b007b3cfc616709a4004f
Sian Richards <sian_richards@hotmail.co.uk>
2025-01-19 14:24:34 +0000

    styling about us page

--- app/routes/$lang.about-us.tsx
+++ app/routes/$lang.about-us.tsx
@@ -16,11 +16,11 @@ const AboutUs = () => {
     <div className="h-screen">
       <NavBar languageStrings={languageStrings} />
       <div className="flex flex-col items-center content-center gap-7 bg-santa-marta bg-cover h-full">
-        <h1 className="text-2xl font-bold mt-3">
+      <h1 className="text-3xl font-extrabold text-white mt-10 text-center">
           {languageStrings["about-us"]}
         </h1>
         <div className="flex justify-center gap-4 h-2/5">
-          <img src="https://placedog.net/300" />
+          <img src="https://placedog.net/300" className="rounded-md" />
           <div className="w-1/2 text-lg text-center content-center bg-white px-2 rounded-md bg-opacity-90">
             <p>{languageStrings["about-us-content"]}</p>
           </div>
